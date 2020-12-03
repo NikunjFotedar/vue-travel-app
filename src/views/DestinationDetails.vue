54fcc91be36241bdac3e0ac3590b9e158fa0ddc7
NikunjFotedar <fotedarnikunj@gmail.com>
2020-12-03 22:35:57 +0530

    added experience in desinations

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,14 +1,32 @@
 <template>
-  <section class="destination">
-    <h1>{{ destination.name }}</h1>
-    <div class="destination-details">
-      <img
-        :src="require(`@/assets/${destination.image}`)"
-        :alt="destination.name"
-      />
-      <p>{{ destination.description }}</p>
-    </div>
-  </section>
+  <div>
+    <section class="destination">
+      <h1>{{ destination.name }}</h1>
+      <div class="destination-details">
+        <img
+          :src="require(`@/assets/${destination.image}`)"
+          :alt="destination.name"
+        />
+        <p>{{ destination.description }}</p>
+      </div>
+    </section>
+    <section class="experiences">
+      <h2>Top experiences in {{ destination.name }}</h2>
+      <div class="cards">
+        <div
+          class="card"
+          v-for="experience in destination.experiences"
+          :key="experience.slug"
+        >
+          <img
+            :src="require(`@/assets/${experience.image}`)"
+            :alt="experience.name"
+          />
+          <span class="card_text">{{ experience.name }}</span>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 <script>
 import store from "@/store.js";
@@ -46,5 +64,32 @@ p {
   margin: 0 40px;
   font-size: 20px;
   text-align: left;
+}
+.experiences {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.cards {
+  display: flex;
+  flex-direction: row;
+}
+.cards img {
+  max-height: 200px;
+}
+
+.card {
+  padding: 0 20px;
+  position: relative;
+}
+.card_text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 25px;
+  font-weight: bold;
+  text-decoration: none;
 }
 </style>
